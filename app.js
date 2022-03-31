e0bcebea5e14755b010b89ad9ce77a19c46acb4c
Eduardo Schlichting <eduardoschlichting@Eduardos-MacBook-Pro.local>
2022-03-31 12:32:32 -0400

    Updating to serve the front end on the back

--- app.js
+++ app.js
@@ -1,73 +1,80 @@
-import express from 'express'; // express package
-import { Server } from 'socket.io'; // socket.io package
-import { createServer } from 'http'; // createServer package
+import express, { application } from "express"; // express package
+import { Server } from "socket.io"; // socket.io package
+import { createServer } from "http"; // createServer package
+import path from "path";
 
 const app = express();
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, { // io engine - allow income requests and distribute chat messages, etc
-    cors: {
-        origin: "*", // opens up the chat server to connections that aren't coming from the same place (it doesn't verify who's connected)
-        methods: ["GET", "POST"]  // cross origin policy (research it)
-    }
+const basePath = process.cwd();
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(basePath, "./dist")));
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
 });
 
-const port = process.env.PORT || 3000;
+const io = new Server(httpServer, {
+  // io engine - allow income requests and distribute chat messages, etc
+  cors: {
+    origin: "*", // opens up the chat server to connections that aren't coming from the same place (it doesn't verify who's connected)
+    methods: ["GET", "POST"], // cross origin policy (research it)
+  },
+});
 
 // cretes an object with all the users stored
 const users = {};
 
-
 httpServer.listen(port, () => {
-    console.log(`Chat server up and running on ${port}`);
+  console.log(`Chat server up and running on ${port}`);
 });
 
 // socket = individual connection coming from the server
-io.on('connection', (socket) => {
-    // console.log('A user has connected');
-    // // this is a custom event we emit on the server side. Can be anything
-    // // id = how the socket tracks each connection
-    io.emit("CONNECTED", socket.id);
+io.on("connection", (socket) => {
+  // console.log('A user has connected');
+  // // this is a custom event we emit on the server side. Can be anything
+  // // id = how the socket tracks each connection
+  io.emit("CONNECTED", socket.id);
 
+  socket.on("user-connected", (data) => {
+    users[socket.id] = data.user;
 
-    socket.on('user-connected', (data) => {
-        users[socket.id] = data.user;
-        
-        console.log(`${users[socket.id]} has connected`);
+    console.log(`${users[socket.id]} has connected`);
 
-        // emits an updated list of users connected to the server
-        io.emit('users-on-server', {user: users});
+    // emits an updated list of users connected to the server
+    io.emit("users-on-server", { user: users });
 
-        socket.broadcast.emit('user-conn-message', {message: `${users[socket.id]} joined`})
+    socket.broadcast.emit("user-conn-message", {
+      message: `${users[socket.id]} joined`,
+    });
 
-        // console.log(users);
-    })
+    // console.log(users);
+  });
 
-    // When a user disconnects, share with the others and delete from the list of users active
-    socket.on('disconnect', () => {
-        // broadcast the event to the other users in the server
-        // io.emit('disconnected', users[socket.id]);
-        console.log(`${users[socket.id]} disconnected`);
-        
-        io.emit('disconnected', {user: socket.id, messageDisc: `${users[socket.id]} left` });
-        // deletes the user from the users array
-        delete users[socket.id];
-    })
+  // When a user disconnects, share with the others and delete from the list of users active
+  socket.on("disconnect", () => {
+    // broadcast the event to the other users in the server
+    // io.emit('disconnected', users[socket.id]);
+    console.log(`${users[socket.id]} disconnected`);
 
-    // listen for incoming messages
-    socket.on('SEND_MESSAGE', function(data){
-        console.log(`${data.user} sent a message: ${data.message}`);
+    io.emit("disconnected", {
+      user: socket.id,
+      messageDisc: `${users[socket.id]} left`,
+    });
+    // deletes the user from the users array
+    delete users[socket.id];
+  });
 
-        // send back to everyone whos connected
-        io.emit('MESSAGE', data);
-    })
+  // listen for incoming messages
+  socket.on("SEND_MESSAGE", function (data) {
+    console.log(`${data.user} sent a message: ${data.message}`);
 
-    socket.on('TYPING', (data) => {
-        console.log(`${data.user} is typing...`);
+    // send back to everyone whos connected
+    io.emit("MESSAGE", data);
+  });
 
-        socket.broadcast.emit('user-typing', data);
-    })
+  socket.on("TYPING", (data) => {
+    console.log(`${data.user} is typing...`);
 
-
-})
-
+    socket.broadcast.emit("user-typing", data);
+  });
+});
